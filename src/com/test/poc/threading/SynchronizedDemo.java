71c40203ec58f44865a087fefb5cf520ce781453
Abhinav Gupta <Abhinav.Gupta>
2020-02-06 11:33:44 +0530

    increment through thread, but it doesnt work sometimes

--- src/com/test/poc/threading/SynchronizedDemo.java
+++ src/com/test/poc/threading/SynchronizedDemo.java
@@ -4,7 +4,14 @@ public class SynchronizedDemo {
 
 	public static void main(String[] args) {
 		Counter counter = new Counter();
-		counter.increment();
+
+		Thread thread = new Thread(() -> {
+			for (int i = 0; i < 1000; i++) {
+				counter.increment();
+			}
+		});
+		thread.start();
+
 		System.out.println("Count " + counter.counter);
 	}
 
