ac698d41ecd3fefaf1e1c75d71263cdcfdf821a3
Abhinav Gupta <Abhinav.Gupta>
2020-02-06 10:48:08 +0530

    Get name of current thread.

--- src/com/test/poc/threading/ThreadDemo.java
+++ src/com/test/poc/threading/ThreadDemo.java
@@ -5,7 +5,7 @@ public class ThreadDemo {
 	public static void main(String[] args) throws InterruptedException {
 		Thread thread = new Thread(() -> {
 			for (int i = 0; i < 5; i++) {
-				System.out.println("thread");
+				System.out.println("thread " + Thread.currentThread().getName());
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
