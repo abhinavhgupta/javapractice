0afcb54eaa668377082ba877e87b8be3d0c02bd2
Abhinav Gupta <Abhinav.Gupta>
2020-05-07 11:33:48 +0530

    Behavioral  Template Method design pattern

---
+++ src/com/example/designpatterns/behavioral/templateMethod/TemplateMethodDemo.java
@@ -0,0 +1,10 @@
+package com.example.designpatterns.behavioral.templateMethod;
+
+public class TemplateMethodDemo {
+
+	public static void main(String[] args) {
+		Generalization algorithm = new Realization();
+		algorithm.findSolution();
+	}
+
+}
